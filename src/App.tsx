e3b3596f3313adce960a00ee5aa8dce566f828fa
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-03-01 12:37:44 +0100

    feat: added rnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./Styles/index.css";
+import { Rnd } from 'react-rnd';
 
 function App() {
   return (
 	<div className="App">
-
+		<Rnd className='windows'
+      bounds="window"
+      maxHeight={450}
+      maxWidth={450}
+      minHeight={200}
+      minWidth={200}
+    >
+      <div style={{height: "100%", width:"100%", backgroundColor:"black"}}></div>
+    </Rnd>
 	</div>
   );
 }
