644501abd4e53e1a29a022416f08e23d8eef18e7
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-03-01 10:32:10 +0100

    feat : added css base files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./Styles/index.css";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+	<div className="App">
+
+	</div>
   );
 }
 
