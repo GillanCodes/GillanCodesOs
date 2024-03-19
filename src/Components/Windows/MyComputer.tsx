f8448c8e83ba0a305d397673a66c9c226a90d91a
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-03-19 12:10:49 +0100

    feat: started a small presentation about me.

--- src/Components/Windows/MyComputer.tsx
+++ src/Components/Windows/MyComputer.tsx
@@ -13,10 +13,11 @@ export const McOp:WindowOptions = {
 }
 
 var age = new Date(Date.now()).getFullYear() - 2003;
+var progy = new Date(Date.now()).getFullYear() - 2017;
 
 export default function MyComputer() {
   return (
-    <div className='window-body'>
+    <div className='window-body' id="mycomputer">
         <menu role='tablist'>
             <li role='tab'><a href='#'>General</a></li>
             <li role='tab'><a href='#'>Devices</a></li>
@@ -24,7 +25,10 @@ export default function MyComputer() {
         </menu>
         <div className="window" role='tabpanel'>
             <div className="window-body">
-                <p>{age}</p>
+                <p className='pres'>Hi ! I am GillanCodes a {age} years old developper !</p>
+                <p>I started programming {progy} years ago in Java and shortly after in PHP.</p>
+                <p>In 2017 I come upon NodeJs and I discorver some framework like "expressJs" or "socketIO" ...</p>
+                <p>Nowadays I work on diffrents type of projects, like WEB, software, games ...</p>
             </div>
         </div>
     </div>
