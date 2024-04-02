023005fb323ea5b35c64db5f0fe2f1d5bcd5f422
GillanCodes <38374481+GillanCodes@users.noreply.github.com>
2024-04-02 12:07:28 +0200

    feat: added projects (need translation)

--- src/Components/Windows/MyProjects.tsx
+++ src/Components/Windows/MyProjects.tsx
@@ -13,44 +13,125 @@ export const MpOp:WindowOptions = {
     y: 320
 }
 
-const data:IProject = {
-  id: "test",
-  desc: "This a a test",
-  icon: "https://cdn.buymeacoffee.com/uploads/profile_pictures/2023/11/uiAClRMyEt5sAt2e.png@300w_0e.webp",
-  name: "Test",
-  year: "2028",
+const sg:IProject = {
+  id: "sg",
+  name: "Stardew Generator",
+  desc: "A small website to generate stardew valley name, farm name and favorite thing !",
+  icon: "sg.png",
+  year: "2024",
   tags: [
     {
-      'name': "test",
-      "color" : "red",
+      'name': "NextJs",
+      "color" : "purple",
     },
     {
-      'name': "test2",
+      'name': "Typescript",
+      "color": "blue"
+    }
+  ]
+}
+
+const RTB:IProject = {
+  id: "RTB",
+  name: "Rev'TonBac",
+  desc: "Un site et une application qui aide les étudiant(e)s dans leur quotidient, avec la possiblité de créer des fiches ou quizz de révisions. Le but est de réduire le stress des examens !",
+  icon: "rtb.png",
+  year: "2020 - 2022",
+  tags: [
+    {
+      'name': "Typescript",
+      "color" : "blue",
+    },
+    {
+      'name': "ReactJs",
+      "color": "purple"
+    },
+    {
+      "name": "MongoDB",
+      "color" : "green"
+    }
+  ]
+}
+
+const waldes:IProject = {
+  id: "Waldes",
+  name: "Wokipédia",
+  desc: "Developpeur bénévole pour le serveur GTARP \"GLife\". Un bot discord dans le but de prendre des demandes d'aide/commerces/recrutements.",
+  icon: "waldes.png",
+  year: "2020",
+  tags: [
+    {
+      'name': "Typescript",
+      "color" : "blue",
+    },
+    {
+      'name': "NodeJS",
+      "color": "green"
+    }
+  ]
+}
+
+const docs:IProject = {
+  id: "docs",
+  name: "Docs Maker",
+  desc: "Un outils pour les developpeurs que vous pouvez self-host. Ce site permet de créer des documentations de ses APIs / Applications pour éviter de perdre du temps.",
+  icon: "",
+  year: "2019",
+  tags: [
+    {
+      'name': "Typescript",
+      "color" : "blue",
+    },
+    {
+      "name": 'ExpressJS',
+      "color": "yellow"
+    },
+    {
+      "name": "MongoDB",
+      "color": "green"
+    }
+  ]
+}
+
+
+const wokipedia:IProject = {
+  id: "wokipedia",
+  name: "Wokipédia",
+  desc: "Bot discord qui intègre des outils de modérations : Channel de Logs / Gestions des Bans & Kicks avec possiblité de mentionné une raison / etc ... Mais également un moyen de gérer ça communauté grace au site web",
+  icon: "wokipedia.png",
+  year: "2017",
+  tags: [
+    {
+      'name': "Javascript",
+      "color" : "yellow",
+    },
+    {
+      'name': "NodeJS",
+      "color": "green"
+    },
+    {
+      "name": "expressJs",
       "color": "yellow"
     }
   ]
 }
 
-const data1:IProject = {
-  id: "test",
-  desc: "This a a test",
-  icon: "https://cdn.buymeacoffee.com/uploads/profile_pictures/2023/11/uiAClRMyEt5sAt2e.png@300w_0e.webp",
-  name: "Test",
-  link: "http://google.com/",
-  year: "2028",
+
+const arkasia:IProject = {
+  id: "arksia",
+  name: "Arkasia",
+  desc: "Je fais mes débuts dans la programmation et je deviens de façon bénévol, au sein d'un serveur minecraft, développeur JAVA",
+  icon: "arkasia.png",
+  year: "2016",
   tags: [
-      {
-        'name': "test",
-        "color" : "purple",
-      },
-      {
-        'name': "test2",
-        "color": "green"
-      },
-      {
-        "name": "test3",
-        'color': "blue"
-      }
+    {
+      'name': "Java",
+      "color" : "red",
+    },
+    {
+      'name': "Graddle",
+      "color": "green"
+    }
   ]
 }
 
@@ -61,9 +142,12 @@ export default function MyProjects() {
       <h2 className='title'>My Projects</h2>
         <div className="projects">
           <div className="cards">
-            <ProjectCard data={data} />
-            <ProjectCard data={data1} />
-            <ProjectCard data={data} />
+            <ProjectCard data={sg} />
+            <ProjectCard data={RTB} />
+            <ProjectCard data={waldes} />
+            <ProjectCard data={docs} />
+            <ProjectCard data={wokipedia} />
+            <ProjectCard data={arkasia} />
           </div>
         </div>
       </div>
